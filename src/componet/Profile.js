33fabb0aa2fa13d50a9f50092e92554584c165aa
Dennis Korir <dkdenniskorir@outlook.com>
2022-01-17 13:12:29 +0300

    completed assignment
    This marks the end of the assignement

--- src/componet/Profile.js
+++ src/componet/Profile.js
@@ -4,8 +4,9 @@ import ProfileImage from '../images/korirdennis.jpg'
 function Profile() {
     return (
         <div>
-            <img className='profile-pricture' src={ProfileImage} alt="Profile" />
+            
             <main className='profile-content'>
+            <img className='profile-pricture' src={ProfileImage} alt="Profile" />
                 <h1 className='profine-name'>Dennis Korir</h1>
                 <p className='profile-role'>Frontend Developer</p>
                 <p className='profile-email'>dennis.korir@brandandtargets.com </p>
@@ -14,10 +15,10 @@ function Profile() {
                 <button className='btn-profile-linkedin'>
                     <i class="fab fa-linkedin"></i> LinkdIn</button>
 
-                <h3>About</h3>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quidem. Quasi, quisquam.  </p>    
-                <h3>Interests</h3>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quidem. Quasi, quisquam.  </p>    
+                <h3 className='profile-title'>About</h3>
+                <p className='profile-text'>I am a frontend developer with a particular interest in making things simple and automating daily tasks. I try to keep up with security and best practices, an dI am always looking for new things to learn</p>    
+                <h3 className='profile-title'>Interests</h3>
+                <p className='profile-text'>Food expert, Music scholar, Reade. Internet fanatic, Bacon buff. Entrepneur. Travel gee. Pop culture ninja. Coffee fanatic.</p>    
                 
                 <footer>
                     <i class="fab fa-twitter-square"></i>   
